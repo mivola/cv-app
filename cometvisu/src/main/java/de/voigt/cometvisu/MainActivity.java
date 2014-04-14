dd953bc2b1e1c13c7e30338da468907537147f91
micha <mivola@gmx.de>
2014-04-14 23:30:29 +0200

    add reload animation fix orientation at startup

--- cometvisu/src/main/java/de/voigt/cometvisu/MainActivity.java
+++ cometvisu/src/main/java/de/voigt/cometvisu/MainActivity.java
@@ -1,27 +1,63 @@
 package de.voigt.cometvisu;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Surface;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
+    public static final String LOADING_MESSAGE = "Lade Seite...";
+
+    final Activity activity = this;
+
     private WebView webView;
 
     private ImageButton reloadBtn;
+
+    private ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int orientation = this.getRequestedOrientation();
+        int rotation = ((WindowManager) this.getSystemService(
+                Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                System.out.println("rotation 0");
+                break;
+            case Surface.ROTATION_90:
+                orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                System.out.println("rotation 90");
+                break;
+            case Surface.ROTATION_180:
+                orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+                System.out.println("rotation 180");
+                break;
+            default:
+                orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+                System.out.println("rotation default");
+                break;
+        }
+
+        this.setRequestedOrientation(orientation);
+
         reloadBtn = (ImageButton) findViewById(R.id.reloadBtn);
         reloadBtn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
+                 pd = ProgressDialog.show(activity, "", LOADING_MESSAGE,true);
                 webView.reload();
              }
         });
@@ -33,8 +69,18 @@ public class MainActivity extends Activity {
             }
         });
 
+        pd = ProgressDialog.show(this, "", LOADING_MESSAGE, true);
+
         webView = (WebView) findViewById(R.id.webView1);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if(pd.isShowing()&&pd!=null)
+                {
+                    pd.dismiss();
+                }
+            }
+        });
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("http://wiregate302/visu-svn");
         //webView.loadUrl("https://mivola.dyndns.org/visu-svn");
