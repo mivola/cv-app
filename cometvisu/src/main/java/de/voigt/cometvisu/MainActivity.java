456312c0d14605395191c57894bdb91bcfa981dd
Micha <mivola@gmx.de>
2015-02-10 10:41:45 +0100

    #2: first draft to avoid reloading webView

--- cometvisu/src/main/java/de/voigt/cometvisu/MainActivity.java
+++ cometvisu/src/main/java/de/voigt/cometvisu/MainActivity.java
@@ -48,106 +48,135 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pd = new ProgressDialog(activity);
-        pd.setMessage(LOADING_MESSAGE);
-        pd.setCancelable(true);
-        pd.setIndeterminate(true);
+        Log.e(APP_NAME, "==== onCreate: " + (savedInstanceState == null ? "null" : "NOT null"));
 
-        setOrientation();
+        if (savedInstanceState != null) {
 
-        reloadBtn = (ImageButton) findViewById(R.id.reloadBtn);
-        reloadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                setOrientation();
-                visuUrl = "";
-                loadSelectedURL();
-            }
-        });
+            Log.e(APP_NAME, "==== savedInstanceState != null 1");
+            webView = (WebView) findViewById(R.id.webView1);
+            Log.e(APP_NAME, "==== savedInstanceState != null: " +webView);
+            webView.restoreState(savedInstanceState);
+            Log.e(APP_NAME, "==== savedInstanceState != null 2");
 
-        reloadBtn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Intent myIntent = new Intent(activity, UrlsListActivity.class);
-                activity.startActivity(myIntent);
+        }else {
 
-                return true;
-            }
-        });
 
-        webView = (WebView) findViewById(R.id.webView1);
-        webView.clearCache(true);
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webView.getSettings().setAppCacheEnabled(false);
-        webView.getSettings().setAppCacheMaxSize(1);
-        getApplicationContext().deleteDatabase("webview.db");
-        getApplicationContext().deleteDatabase("webviewCache.db");
 
-        webView.setWebChromeClient(new WebChromeClient() {
-            public void onConsoleMessage(String message, int lineNumber, String sourceID) {
-                Log.e(APP_NAME, message + " -- From line "
-                        + lineNumber + " of "
-                        + sourceID);
-            }
 
-            public boolean onConsoleMessage(ConsoleMessage cm) {
-                Log.e(APP_NAME, cm.message() + " -- From line "
-                        + cm.lineNumber() + " of "
-                        + cm.sourceId());
-                return true;
-            }
-        });
+            pd = new ProgressDialog(activity);
+            pd.setMessage(LOADING_MESSAGE);
+            pd.setCancelable(true);
+            pd.setIndeterminate(true);
 
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (pd != null && pd.isShowing()) {
-                    try {
-                        pd.dismiss();
-                    } catch (Throwable t) {
-                        // this happens on 4.4.4 in landscape orientation... Log.d("MyApplication", "error while dismissing pd", t);
+            setOrientation();
+
+            reloadBtn = (ImageButton) findViewById(R.id.reloadBtn);
+            reloadBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setOrientation();
+                    visuUrl = "";
+                    loadSelectedURL();
+                }
+            });
+
+            reloadBtn.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    Intent myIntent = new Intent(activity, UrlsListActivity.class);
+                    activity.startActivity(myIntent);
+
+                    return true;
+                }
+            });
+
+            webView = (WebView) findViewById(R.id.webView1);
+            webView.clearCache(true);
+            webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+            webView.getSettings().setAppCacheEnabled(false);
+            webView.getSettings().setAppCacheMaxSize(1);
+            getApplicationContext().deleteDatabase("webview.db");
+            getApplicationContext().deleteDatabase("webviewCache.db");
+
+            webView.setWebChromeClient(new WebChromeClient() {
+                public void onConsoleMessage(String message, int lineNumber, String sourceID) {
+                    Log.e(APP_NAME, message + " -- From line "
+                            + lineNumber + " of "
+                            + sourceID);
+                }
+
+                public boolean onConsoleMessage(ConsoleMessage cm) {
+                    Log.e(APP_NAME, cm.message() + " -- From line "
+                            + cm.lineNumber() + " of "
+                            + cm.sourceId());
+                    return true;
+                }
+            });
+
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    if (pd != null && pd.isShowing()) {
+                        try {
+                            pd.dismiss();
+                        } catch (Throwable t) {
+                            // this happens on 4.4.4 in landscape orientation... Log.d("MyApplication", "error while dismissing pd", t);
+                        }
                     }
                 }
-            }
 
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                super.onReceivedError(view, errorCode, description, failingUrl);
-                Log.e(APP_NAME, description + " -- From line; errorCode: " + errorCode);
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String newUrl) {
-                String currentlySelectedUrl = getCurrentlySelectedUrl();
-//                if (Uri.parse(url).getHost().equals(Uri.parse(currentlySelectedUrl).getHost()) &&
-  //                      (Uri.parse(url).getPort() == Uri.parse(currentlySelectedUrl).getPort())) {
-                if(newUrl.startsWith(currentlySelectedUrl)){
-                    // this belongs to the currently selected page
-                    return false;
+                @Override
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                    super.onReceivedError(view, errorCode, description, failingUrl);
+                    Log.e(APP_NAME, description + " -- From line; errorCode: " + errorCode);
                 }
-                // let the system handle all other links
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(newUrl));
-                startActivity(intent);
-                return true;
-            }
 
-        });
-        webView.getSettings().setJavaScriptEnabled(true);
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String newUrl) {
+                    String currentlySelectedUrl = getCurrentlySelectedUrl();
+//                if (Uri.parse(url).getHost().equals(Uri.parse(currentlySelectedUrl).getHost()) &&
+                    //                      (Uri.parse(url).getPort() == Uri.parse(currentlySelectedUrl).getPort())) {
+                    if (newUrl.startsWith(currentlySelectedUrl)) {
+                        // this belongs to the currently selected page
+                        return false;
+                    }
+                    // let the system handle all other links
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(newUrl));
+                    startActivity(intent);
+                    return true;
+                }
 
-        loadSelectedURL();
+            });
+            webView.getSettings().setJavaScriptEnabled(true);
 
-        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        ScreenReceiver screenReceiver = new ScreenReceiver(webView);
-        registerReceiver(screenReceiver, filter);
+            loadSelectedURL();
 
+            IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+            filter.addAction(Intent.ACTION_SCREEN_OFF);
+            ScreenReceiver screenReceiver = new ScreenReceiver(webView);
+            registerReceiver(screenReceiver, filter);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.e(APP_NAME, "==== onSaveInstanceState");
+        webView.saveState(outState);
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.e(APP_NAME, "==== onRestoreInstanceState");
+  //      webView.restoreState(savedInstanceState);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
+        Log.e(APP_NAME, "==== onWindowFocusChanged: "+ hasFocus);
         if (hasFocus) {
-            loadSelectedURL();
+//            loadSelectedURL();
         }
     }
 
