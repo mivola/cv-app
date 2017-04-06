1dac8fd83a8520eea88417fae205bdac37aa5237
Michael Voigt <michael.voigt@softwareag.com>
2017-04-06 21:16:01 +0200

    #5: fix the failing JUnit test

--- cometvisu/src/main/java/de/voigt/cometvisu/CometVisuApp.java
+++ cometvisu/src/main/java/de/voigt/cometvisu/CometVisuApp.java
@@ -4,12 +4,15 @@ import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+
 public class CometVisuApp extends Application {
 
     private BaseActivity currentActivity = null;
 
     public void onCreate() {
         super.onCreate();
+        FirebaseApp.initializeApp(this);
     }
 
     public BaseActivity getCurrentActivity() {
